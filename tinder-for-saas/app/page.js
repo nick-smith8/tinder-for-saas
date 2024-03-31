4318aff59856f9b9a22d6a501470e5c7bd7da2ee
nick-smith8 <nick@hitropic.com>
2024-03-31 09:56:07 -0600

    add styling / toast for yes/no

--- tinder-for-saas/app/page.js
+++ tinder-for-saas/app/page.js
@@ -8,12 +8,48 @@ const initialSuppliers = [
 
 function Card({ supplier }) {
   return (
-    <div className="block max-w-sm p-6 cursor-pointer bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div className="block w-[300px] p-6 cursor-pointer bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <p>{supplier.name}</p>
       <img src={supplier.logo} alt={`${supplier.name} logo`} />
     </div>
   );
 }
+const showToast = (supplierName, side) => {
+  // Create a toast element
+  const toast = document.createElement("div");
+  toast.classList.add("toast");
+  const statements = {
+    left: [
+      "Swiped left? Maybe they were too cloud-nine for your taste!",
+      "Not feeling the spark? It's not you, it's their API.",
+      "Swiped left? They must've not had the right features!",
+      "Not a match, huh? There's plenty of SaaS in the sea!",
+    ],
+    right: [
+      "Bingo! This vendor's got the code to your heart.",
+      "Swiped right? Prepare for a lifetime of updates and integrations!",
+      "Swiped right? Looks like it's a match made in cloud heaven!",
+      "A perfect match! Get ready for some seamless integration.",
+    ],
+  };
+
+  const randomIndex = Math.floor(Math.random() * statements[side].length);
+  toast.textContent = statements[side][randomIndex];
+  toast.style.position = "absolute";
+  toast.style.top = "5vh";
+  toast.style.left = "80vh";
+  toast.style.border = "2px solid #333";
+  toast.style.backgroundColor =
+    side === "left" ? "rgb(252 165 165)" : "rgb(187 247 208)";
+  toast.style.borderRadius = "10px";
+  // Append toast to the body to float over content
+  document.body.appendChild(toast);
+
+  // Remove toast after 3 seconds
+  setTimeout(() => {
+    document.body.removeChild(toast);
+  }, 3000);
+};
 
 export default function Home() {
   const [suppliers, setSuppliers] = useState(initialSuppliers);
@@ -24,7 +60,7 @@ export default function Home() {
 
   const handleDrop = (e, side) => {
     const supplierName = e.dataTransfer.getData("text");
-    alert(`You swiped ${supplierName} to the ${side}`);
+    showToast(supplierName, side);
     e.target.style.backgroundColor = "";
     setSuppliers(
       suppliers.filter((supplier) => supplier.name !== supplierName)
@@ -33,7 +69,7 @@ export default function Home() {
 
   const allowDrop = (e, side) => {
     e.target.style.backgroundColor =
-      side === "left" ? "rgb(220 38 38)" : "#A3C63C";
+      side === "left" ? "rgb(220 38 38)" : "rgb(22 163 74)";
     e.preventDefault();
   };
 
@@ -47,11 +83,11 @@ export default function Home() {
       >
         Never would I
       </div>
-      <div className="flex-1 flex flex-col gap-4">
+      <div className="flex-1 flex flex-col gap-4 items-center">
         <img
           src="https://assets-global.website-files.com/642e31b6a69b3e0852108536/642e36bdbdb822030f521018_tropic-logo-dark.svg"
           alt="Tropic logo"
-          className="w-1/2 mx-auto"
+          className="w-1/2 mx-auto mt-4"
         />
         {suppliers.map((supplier, index) => (
           <div
@@ -64,7 +100,7 @@ export default function Home() {
         ))}
       </div>
       <div
-        className="flex-1 flex bg-green-200"
+        className="flex-1 flex  bg-green-200"
         onDrop={(e) => handleDrop(e, "right")}
         onDragOver={(e) => allowDrop(e, "right")}
         onDragLeave={(e) => (e.target.style.backgroundColor = "")}
